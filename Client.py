3206e999e6bf8e5fcee5712ea5ddf905f50a91e5
wang <13961252501@139.com>
2019-02-26 02:18:21 -0800

    FTP盘项目

---
+++ Client.py
@@ -0,0 +1,106 @@
+﻿import os
+import socket
+import threading
+import sys
+import time
+
+class FtpClient(object):
+    def __init__(self, ADDR):
+        self.Socket = socket.socket()
+        self.addr = ADDR
+    def Menu(self):
+        print('=' * 40 + '命令集' + '=' * 40)
+        print('*' * 37 + '1、查看目录' + '*' * 38)
+        print('*' * 37 + '2、下载文件' + '*' * 38)
+        print('*' * 37 + '3、上传文件' + '*' * 38)
+        print('*' * 37 + '4、退    出' + '*' * 38)
+    def Exit(self):
+        self.Socket.send('exit'.encode())
+        sys.exit('客户端退出!')
+    def View(self):
+        self.Socket.send('view'.encode())
+        data = self.Socket.recv(40000)
+        print()
+        print()
+        print('文件目录如下:')
+        filelist = data.decode().split('#')
+        self.filedict = {}
+        count = 1
+        for i in filelist:
+            self.filedict[count] = i
+            print(count, '、', i,sep = '')
+            count += 1
+    def DownLoad(self):
+        self.Socket.send('download'.encode())
+        self.View()
+        cmd = input('你想下载哪个文件，输入序号:')
+        try:
+            filename = self.filedict[int(cmd)]                    #通过字典索引文件名
+            print(filename)
+            self.Socket.send(filename.encode())
+        except:
+            print('输入有误，已跳回主菜单!')
+            return None
+        try:
+            f = open(filename,'wb')
+        except:
+            print('打开文件失败!')
+        else:
+            while True:
+                data = self.Socket.recv(1024)
+                if data == b'######':
+                    break
+                f.write(data)
+            f.close()
+    def UpLoad(self):
+        self.Socket.send('upload'.encode())
+        FileList = os.listdir()
+        for i in FileList:
+            print(FileList.index(i) + 1 , '、' , i)
+        print('文件列表已列出，你想传哪一个文件至云端?\n由于制作简陋，不支持文件夹上传，请上传有后缀的文件!')
+        file = input()
+        filename = FileList[int(file) - 1]                         #获取已上传的文件名
+        self.Socket.send(filename.encode())
+        try:
+            f = open(filename,'rb')
+        except:
+            print('打开文件失败,返回主菜单！')
+        else:#循环读取文件，并上传到云端
+            while True:
+                data = f.read(1024)
+                if not data:
+                    time.sleep(0.1)
+                    self.Socket.send('##'.encode())
+                    break
+                self.Socket.send(data)
+            f.close()
+        print('上传完毕！\n\n')
+    def client_forever(self):
+        try:
+            self.Socket.connect(self.addr)
+        except:
+            sys.exit('连接服务器失败，已退出!')
+        self.Socket.send('R'.encode())
+        data = self.Socket.recv(1024)
+        if data.decode() == 'ok':
+            while True:
+                self.Menu()
+                cmd = input('输入你的指令:')
+                if cmd == '1':
+                    self.View()
+                if cmd == '2':
+                    self.DownLoad()
+                if cmd == '3':
+                    self.UpLoad()
+                if cmd == '4':
+                    self.Exit()
+
+def main():
+    ADDR = ('129.211.125.18',8421)
+    A = FtpClient(ADDR)
+    A.client_forever()
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ Server.py
@@ -0,0 +1,104 @@
+﻿import os
+import socket
+import threading
+import sys
+import time
+#自定义FTP服务器类
+class FtpServer(object):
+    def __init__(self, ADDR, PATH):
+        self.addr = ADDR
+        self.ip = ADDR[0]
+        self.port = ADDR[1]
+        self.path = PATH
+        self.Socket = socket.socket()
+    def server_forever(self):
+        self.Socket.bind(self.addr)
+        print('监听端口%s中' % str(self.addr))
+        self.Socket.listen(5)
+        while True:
+            c, addr = self.Socket.accept()
+            print('客户端%s已连接' % str(addr))
+            t = threading.Thread(target = self.ThreadServer, args = (c,))
+            t.Daemon = True
+            t.start()
+    def ThreadServer(self, *c):
+        c = c[0]
+        while True:
+#            print('等待客户端命令:')
+            request = c.recv(1024)
+            request = request.decode()
+#            print(request)
+            if request == 'R':
+#                print('请求连接信息:')
+                self.MakeSure(request, c)
+            if request == 'view':
+                self.View(c)
+            if request == 'exit':
+                print('客户端已退出，关闭%s线程!' % str(c))
+                return None
+            if request == 'download':
+                self.DownLoad(c)
+            if request == 'upload':
+                self.UpLoad(c)
+            else:
+                pass
+    def MakeSure(self, request, c):
+        time.sleep(0.1)
+        c.send('ok'.encode())
+#        print('发送OK')
+    def View(self, c):
+#        print('调用文件列表函数!')
+        FtpList = os.listdir(self.path)
+        data = '#'.join(FtpList)
+        time.sleep(0.1)
+        c.send(data.encode())
+    def DownLoad(self, c):
+        self.View(c)
+#        print('准备下载')
+        filename = c.recv(1024)
+        filename = filename.decode()
+        filename = c.recv(1024)
+        filename = filename.decode()
+#        print(filename)
+        try:
+            f = open(self.path + filename, 'rb')
+        except OSError:
+            pass
+#            print('文件打开失败!')
+        else:
+            while True:
+                data = f.read(1024)
+                if not data:
+                    time.sleep(1)
+                    c.send('######'.encode())
+                    break
+                time.sleep(0.1)
+                c.send(data)
+            f.close()
+    def UpLoad(self, c):
+        print('等待接收文件名...')
+        filename = c.recv(1024)
+        filename = filename.decode()
+        print(filename)
+        try:
+            f = open(self.path + filename,'wb')
+        except:
+            print('文件打开失败!')
+        else:
+            while True:
+                data = c.recv(1024)
+                if data == b'######':
+                    break
+                f.write(data)
+            f.close()
+        print('上传程序完毕!')
+def main():
+    PATH = "C:\\Users\\Administrator\\Desktop\\Ftp\\FtpFile\\"
+    IP = '172.17.0.4'
+    PORT = 8421
+    ADDR = (IP, PORT)   
+    A = FtpServer(ADDR, PATH)
+    A.server_forever()
+
+if __name__ == '__main__':
+    main()
